48d6f7060fde3394f5b55e61632863734ba29e39
gotaeh <ktt2341@naver.com>
2023-08-07 19:05:30 +0900

    project_3

--- js/main_script.js
+++ js/main_script.js
@@ -1,8 +1,8 @@
-var xhttpHeader = new XMLHttpRequest();
-xhttpHeader.onreadystatechange = function() {
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
     if(this.readyState == 4 && this.status == 200) {
         document.getElementById("header_container").innerHTML = this.responseText;
     }
 };
-xhttpHeader.open("GET", "/header", true);
-xhttpHeader.send();
+xhttp.open("GET", "/header", true);
+xhttp.send();
