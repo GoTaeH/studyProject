412dba72f71df48b0303543d489da37ae542cf0f
gotaeh <ktt2341@naver.com>
2023-08-07 20:22:10 +0900

    3

--- index.js
+++ index.js
@@ -20,28 +20,28 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/html/main.html'));
 });
 app.get('/header', (req, res) => {
-    res.sendFile(__dirname + '/html/header.html');
+    res.sendFile(path.join(__dirname + '/html/header.html'));
 });
 
 app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/html/login.html');
+    res.sendFile(path.join(__dirname + '/html/login.html'));
 });
 app.get('/membership', (req, res) => {
-    res.sendFile(__dirname + '/html/membership.html');
+    res.sendFile(path.join(__dirname + '/html/membership.html'));
 });
 
 app.get('/bookmark', (req, res) => {
-    res.sendFile(__dirname + '/html/bookmark.html');
+    res.sendFile(path.join(__dirname + '/html/bookmark.html'));
 });
 
 app.get('/style/:name', (req, res) => {
-    res.sendFile(__dirname + '/style/' + req.params.name);
+    res.sendFile(path.join(__dirname + '/style/' + req.params.name));
 });
 app.get('/image/:name', (req, res) => {
-    res.sendFile(__dirname + '/image/' + req.params.name);
+    res.sendFile(path.join(__dirname + '/image/' + req.params.name));
 });
 app.get('/js/:name', (req, res) => {
-    res.sendFile(__dirname + '/js/' + req.params.name);
+    res.sendFile(path.join(__dirname + '/js/' + req.params.name));
 });
 
 
