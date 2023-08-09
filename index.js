e9448e9beb755d89880fcaad980fac8eabf922cc
gotaeh <ktt2341@naver.com>
2023-08-09 13:51:54 +0900

    3

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 #!/usr/bin/node
 
 const express = require('express');
-// const session = require('express-session');
 const app = express();
 const port = 3000;
 
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
 const path = require('path');
 const cors = require('cors');
-// const mysql = require('mysql');
-// const dbconfig = require('../config/dbinfo.js');
-// const connection = mysql.createConnection(dbconfig);
+const { createConnection } = require('net');
 
-// app.use(session({
-//     secret: 'lsakd!dfs#$asd',
-//     resave: false,
-//     saveUninitialized: true
-// }));
+const mysql = require('mysql');
+const dbconfig = require('./config/dbinfo.js');
+
+const { error } = require('console');
+const connection = mysql.createConnection(dbconfig);
+
+app.use(express.json());    // 요청 본문을 JSON으로 파싱
+app.use(express.urlencoded({ extended: true})); // 폼 데이터 파싱
+app.use(cookieParser());
+app.use(session({
+    secret: 'lsakd!dfs#$asd',
+    resave: false,
+    saveUninitialized: true,
+    // cookie: {
+    //     maxAge: 3600000 //세션 유효 기간 : 1시간
+    // }
+}));
 
 app.use(
     cors({
@@ -24,23 +36,49 @@ app.use(
     })
 );
 
-// // 로그인 로직
-// app.post('/login', (req, res) => {
-    
-// })
-// // 로그아웃 로직
-// app.get('/logout', (req, res) => {
-//     req.session.destroy((err) => {
-//         if(err) {
-//             console.error(err);
-//         } else {
-//             res.redirect('/');
-//         }
-//     });
-// });
+// 로그인 로직
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
 
+    if (req.session.user && req.session.user.email === email) {
+        res.redirect('/');
+    }
+    const query = 'SELECT * FROM member WHERE email = ? AND password = ?';
+    connection.query(query, [email, password], (error, rows) => {
+        if (error) {
+            console.error(error);
+            return res.status(500).send('서버 오류 발생');
+        }
+        if (rows.length === 0) {
+            return res.status(401).send('이메일 또는 비밀번호가 틀렸습니다.');
+        }
+        req.session.user = { email };
+        res.setHeader('Set-Cookie', ['user=' + email]);
+        res.redirect('/');
+    });
+});
+
+// 로그아웃 로직
+app.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if(err) {
+            console.error(err);
+        } else {
+            res.clearCookie('user');
+            res.redirect('/');
+        }
+    });
+});
+
+app.get('/api', (req, res) => {
+    connection.query('SELECT * FROM member', (error, rows) => {
+        if(error) throw error;
+        res.send(rows);
+    });
+});
 
 app.get('/', (req, res) => {
+    console.log(req.session);
     res.sendFile(path.join(__dirname, '/html/main.html'));
 });
 app.get('/header', (req, res) => {
