3304bc5020d5e520d98a45bca3ac10e0dd6d06d2
gotaeh <ktt2341@naver.com>
2023-08-12 04:20:51 +0900

    project_4

--- js/l_script.js
+++ js/l_script.js
@@ -1,3 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const errorMessage = urlParams.get('error');
+    
+    if (errorMessage === 'authFailed') {
+        const errorMessageP = document.getElementById('error-message');
+        errorMessageP.textContent = '이메일 또는 비밀번호가 틀렸습니다.';
+        errorMessageP.style.display = 'block';
+    }
+});
+
 function memOpen() {
     window.open('/membership', 'join the membership', 'width=700, height=700');
 }
--- js/main_script.js
+++ js/main_script.js
@@ -1,8 +1,33 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if(this.readyState == 4 && this.status == 200) {
-        document.getElementById("header_container").innerHTML = this.responseText;
+// var xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange = function() {
+//     if(this.readyState == 4 && this.status == 200) {
+//         document.getElementById("header_container").innerHTML = this.responseText;
+//     }
+// };
+// xhttp.open("GET", "/header", true);
+// xhttp.send();
+
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const response = await fetch('/api', {
+            method: 'GET',
+            credentials: 'include' // 이 옵션을 설정하여 쿠키를 함께 보냄
+        });
+
+        const urlParams = new URLSearchParams(window.location.search);
+        const isLogged = urlParams.has('logged'); // 쿼리 파라미터 확인
+
+        if (response.ok) {
+            const user = await response.json();
+            const headerContainer = document.getElementById('header_container');
+
+            if (user.email || isLogged) { // 로그인되었거나 isLogged가 true일 때
+                headerContainer.innerHTML = await fetch('/header2').then(response => response.text());
+            } else {
+                headerContainer.innerHTML = await fetch('/header1').then(response => response.text());
+            }
+        }
+    } catch (error) {
+        console.error(error);
     }
-};
-xhttp.open("GET", "/header", true);
-xhttp.send();
+});
